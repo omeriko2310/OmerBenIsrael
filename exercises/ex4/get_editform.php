242e8f16ca6fa416ff1c1b2d9a675d5f351e1250
omeriko2310 <96539420+omeriko2310@users.noreply.github.com>
2023-05-08 21:21:04 +0300

    Add files via upload

---
+++ exercises/ex4/get_editform.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forms</title>
+</head>
+<body>
+    <h2>
+    Order recieved! 
+    <br><br>
+    Order details:<br>
+    </h2>
+    <h2>Shirt Color: <?php echo $_GET["color"]; ?></h2>
+    <h3>
+        Delivery Will Take Up To 5 Business Days. 
+    </h3>
+</body>
+</html>
