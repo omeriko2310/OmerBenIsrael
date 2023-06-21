bba5b7b137b8516cef8b167f30d70573132fff42
omeriko2310 <96539420+omeriko2310@users.noreply.github.com>
2023-06-21 16:59:13 +0300

    Delete exercises/ex5/bookStore directory

--- exercises/ex5/bookStore/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('#category-list').on('change', function () {
-      var selectedCategory = $(this).val();
-      var url = 'index.php?category=' + selectedCategory;
-      window.location.href = url;
-  });
-});
